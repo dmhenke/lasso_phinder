01f32cc385240a309a5c1ca897a59dd5d16bdbeb
lkmklsmn <lkmklsmn@gmail.com>
2024-04-01 15:36:31 -0500

    Update all functions
    - Switch to minimum lambda vs 1stdev. 1stdev more prone to returning all zero coefficients.
    - Pick RMSE as model performance metric (vs correlation). Correlation prone to produce NAs when there is variance in predicted values

--- Code/allfunctions.R
+++ Code/allfunctions.R
@@ -23,54 +23,64 @@ find_lambda <- function(X, y, plot = T){
     alpha = 1, 
     lambda = NULL)
   if(plot) plot(fitcv, xvar = "lambda", label = T)
-  # fitcv$lambda.min
-  fitcv$lambda.1se # more conservative vs 'lambda.min'
+  
+  fitcv$lambda.min # more conservative vs 'lambda.min'
 }
 
 ## Given matrix of correlation between predicted and observed values from
 # cross-validation across range of phi values, find best phi ####
-find_best_phi <- function(correls, phi_range, plot = T){
-  median_correls <- unlist(lapply(split(correls$cor,correls$phi), median))
-  median_rmse <- unlist(lapply(split(correls$rmse,correls$phi), median))
+find_best_phi_rmse <- function(correls, phi_range, plot = T){
+  median_rmse <- apply(correls, 1, median)
   
   aframe <- data.frame(
     phi = phi_range, 
-    cor = median_correls,
     rmse = median_rmse)
   
-  afit <- lm(cor ~ phi, data = aframe[c(1, nrow(aframe)), ])
+  afit <- lm(rmse ~ phi, data = aframe[c(1, nrow(aframe)), ])
   preds <- predict(afit, aframe)
-  diff_corr <- aframe$cor - preds
-  afit2 <- lm(rmse ~ phi, data = aframe[c(1, nrow(aframe)), ])
-  preds2 <- predict(afit2, aframe)
   diff_rmse <- aframe$rmse - preds2
   
-  best_cor_phi <- phi_range[which(diff_corr == max(diff_corr))]
   best_rmse_phi <- phi_range[which(diff_rmse == min(diff_rmse))]
   
   if(plot){
-    aframe$size=1;aframe$size[aframe$phi%in% c(best_cor_phi,best_rmse_phi)]<-2
-    ggplot(aframe, aes(rmse, cor,color=phi,size=size)) +
+    ggplot(aframe, aes(phi, rmse)) +
       labs(
-        x = 'RSME',
-        y = 'Correlation'
+        x = 'Phi',
+        y = 'RMSE'
       ) +
       geom_point() +
-      geom_abline(intercept =afit$coefficients[1],slope = afit$coefficients[2] )+
-      theme_classic()
-    diffame <-data.frame(diff_corr,diff_rmse,phi=phi_range,size=1)
-    diffame$size=1;diffame$size[diffame$phi%in% c(best_cor_phi,best_rmse_phi)]<-2
-    ggplot(diffame, aes(diff_rmse, diff_corr,color=phi,size=size)) +
-      labs(
-        x = 'diff RSME',
-        y = 'diff Correlation'
-      ) +
-      geom_point() +
-      geom_abline(intercept =afit$coefficients[1],slope = afit$coefficients[2] )+
+      geom_vline(xintercept = best_rmse_phi) +
       theme_classic()
   }
   
-  return(data.frame(best_cor_phi,best_rmse_phi))
+  return(best_rmse_phi)
+}
+
+# Get rmse values from cross-validation ####
+get_rmse_from_xvalid <- function(X, y, penalties){
+  asplits <- suppressWarnings(split(sample(1:nrow(X)), 1:n_folds))
+  
+  correls <- do.call(cbind, lapply(names(asplits), function(x){
+    train <- unlist(asplits[setdiff(names(asplits), x)])
+    test <- unlist(asplits[x])
+    
+    do.call(rbind, lapply(phi_range, function(phi){
+      lasso_tr <- glmnet(
+        X[train,],
+        y[train],
+        lambda = lambda_min,
+        penalty.factor = 1 - penalties * phi)
+      
+      pred <- predict(lasso_tr, X[test,])
+      rmse <- sqrt(apply((y[test] - pred)^2, 2, mean))
+      
+      rmse
+    }))
+  }))
+  correls <- apply(correls, 2, function(x)
+    (x - mean(x))/sd(x))
+  
+  return(correls)
 }
 
 ## Run lasso ####
@@ -79,51 +89,34 @@ run_reg_lasso <- function(X, y, scores,
                           phi_range = seq(0, 1, length = 30)){
   lambda_min <- find_lambda(X, y, plot = F)
   
-  # print(paste("Lambda min:", lambda_min))
-  print(paste("Lambda 1st SE:", round(lambda_min,4)))
+  print(paste("Lambda min:", round(lambda_min,4)))
   
   afit <- glmnet(
     X, y, 
     alpha = 1, 
     lambda = lambda_min)
   betas <- afit$beta[,1]
-  # sniff test #
+  
   if(sum(betas) == 0){
     print("All betas are zero.")
     return(NA)
   }
+  
   penalties <- scores[match(colnames(X), names(scores))]
   names(penalties) <- colnames(X)
   penalties[is.na(penalties)] <- 0
   penalties <- penalties/max(scores)
   
-  asplits <- suppressWarnings(split(sample(1:nrow(X)), 1:n_folds))
+  correls <- get_rmse_from_xvalid(X, y, penalties)
   
-  correls <- do.call(rbind, lapply(names(asplits), function(x){
-    train <- unlist(asplits[setdiff(names(asplits), x)])
-    test <- unlist(asplits[x])
-    
-    do.call(rbind,lapply(phi_range, function(phi){
-      lasso_tr <- glmnet(
-        X[train,],
-        y[train],
-        lambda = lambda_min,
-        penalty.factor = 1 - penalties * phi)
-      
-      pred <- predict(lasso_tr,X[test,])
-      rmse <- sqrt(apply((y[test]-pred)^2,2,mean))
-      corout <- try(cor(pred, y[test]),silent = T)
-      data.frame(cor=cor(pred, y[test]) ,rmse=rmse,run=x,phi=phi)
-    }))
-  }))
   if(length(unique(dim(correls) == dim(na.omit(correls)))) == 2){
     print("Missing values in correlation.")
     return(NA)
   }
   
-  best_phi <- find_best_phi(correls, phi_range)
-  best_phi_cor <- best_phi$best_cor_phi
-  print(paste("Phi best:", round(best_phi_cor,4)))
+  best_phi <- find_best_phi_rmse(correls, phi_range, plot = F)
+
+  print(paste("Best phi based on RMSE:", round(best_phi, 4)))
   
   # Update lambda and rerun LASSO
   afit <- glmnet(
@@ -131,12 +124,10 @@ run_reg_lasso <- function(X, y, scores,
     y,
     alpha = 1,
     lambda = lambda_min,
-    penalty.factor = 1 - penalties * best_phi_cor)
+    penalty.factor = 1 - penalties * best_phi)
   betas_pen <- afit$beta[,1]
   
-
   return(list(best_phi,
-              best_phi_cor,
               correlations = correls,
               betas = data.frame(betas, betas_pen)))
 }
