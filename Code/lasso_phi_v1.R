28d4cea67732f369f6c18ae0a3c70f53dc892ced
dmhenke <davidmhenke@gmail.com>
2024-04-17 08:50:46 -0600

    update to include more expression data sources (rna, cnv) and scores (krono, demeter2)

--- Code/lasso_phi_v1.R
+++ Code/lasso_phi_v1.R
@@ -20,7 +20,7 @@ ppi$gene1 <- gene_info$hgnc_symbol[match(ppi$protein1, gene_info$ensembl_peptide
 ppi$gene2 <- gene_info$hgnc_symbol[match(ppi$protein2, gene_info$ensembl_peptide_id)]
 
 
-# Load DepMap data ####
+# Load data: expressions & scores ####
 # load("C:/Users/Dafydd/Documents/Projects/lasso_PPI/global.RData")
 load(paste0("../Data/global.RData"))
 
@@ -30,43 +30,70 @@ source("./allfunctions.R")
 
 
 # Define X and y ####
-ok_cells <- intersect(
-  rownames(rnaseq),
-  rownames(kronos))
+# Dependency score source
+# Y
+scor_src <- 'demeter2'
+if(scor_src == 'demeter2'){
+  dep_scor <- demeter2} else if(scor_src == 'kronos'){
+    dep_scor <- kronos}
+# X
+exp_src <- 'cnv'
+if(exp_src=='rnaseq'){
+  ind_exp <- rnaseq
+}else if(exp_src=='cnv'){
+  ind_exp <- na.omit(cnv)
+}
 
-X <- rnaseq[ok_cells, ]
-y <- kronos[ok_cells,]
+ind_exp <- na.omit(ind_exp)
+
+
+ok_cells <- intersect(
+  rownames(ind_exp),
+  rownames(dep_scor))
+
+X <- ind_exp[ok_cells, ]
+y <- dep_scor[ok_cells,]
+if(exp_src=='rnaseq'){
 expressed <- apply(X, 2, function(x) mean(x > 0))
 X <- X[, expressed > 0.95]
 X <- apply(X, 2, function(x)
   (x - mean(x))/sd(x))
-
+}
 
 # test phi, corr vs rmse
-gene_sample <- c("MYC",sample(colnames(kronos),100))
+gene_sample <- c("MYC",sample(colnames(dep_scor),100))
 gene_sample2 <- c("KDM5D","SOX10","FAM50A","RPP25L","PAX8","KRTAP4-11",
                   "EBF1","IRF4","H2BC15","MYB","MDM2","OR4P4"    ,
                   "KRAS","NRAS","HNF1B","OR4C11","EIF1AX","POU2AF1",  
                   "TP63","BRAF","TTC7A","OR4S2")
-gene_sample <-gene_sample[gene_sample%in%colnames(kronos)]
-gene_sample2 <-gene_sample2[gene_sample2%in%colnames(kronos)]
+gene_sample <-gene_sample[gene_sample%in%colnames(dep_scor)]
+gene_sample2 <-gene_sample2[gene_sample2%in%colnames(dep_scor)]
+
+# BRCA1 assocated genes
+gene_sample3 <-c("PALB2","BARD1","BRIP1","RAD50","RAD51C","RAD51D","RAD54L","ATM","ATR","ATRX","FANCONI","FANCA", "FANCB", "FANCC","CHEK1", "CHEK2", "BLM","NBN","MRE11A")
+gene_sample3 <-gene_sample3[gene_sample3%in%colnames(dep_scor)]
+
+gene_sample4 <- c("MYC","SF3B1","BRCA1","BRCA2")
+
+
 
 
 library("parallel")
 
-phiout <- mclapply(gene_sample2,function(gene){
+phiout <- mclapply(gene_sample4,function(gene){
   save_results_file <- paste0(gene,'.RData')
-  if(save_results_file%in%list.files(paste0("../Outputs/"))){
+  if(save_results_file%in%list.files(paste0("../Outputs/",scor_src,"/"))){
     print(paste('alredy processed',gene))
     NULL
   }else{
     print(paste('Processing',gene))
-    y <- y[, gene]
+    y <- na.omit(y[, gene])
+    X <- X[match(names(y),rownames(X)), ]
     scores <- get_scores(gene, ppi)
     out <- run_reg_lasso(
       X, y, scores,
       n_folds = 10, phi_range = seq(0, 1, length = 30))
-    save(out,file=paste0("../Outputs/",gene,'.RData'))
+    save(out,file=paste0("../Outputs/",scor_src,"/",gene,'.RData'))
     return(out)
   }
 },mc.cores = 10)
