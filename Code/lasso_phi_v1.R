b0b418f2315a8edd49e4efbb76f1a0c67aa17f00
dmhenke <davidmhenke@gmail.com>
2024-05-08 11:38:55 -0600

    "lasso_phi_v1.R" added more gene/chr info to ppi obj "lasso_phi_v2_miko.R" using miko's code and adding features: logic to save, skip saved, id score obj used, and plot results

--- Code/lasso_phi_v1.R
+++ Code/lasso_phi_v1.R
@@ -6,19 +6,25 @@ library(glmnet)
 
 
 # Load String data ####
+# ppi <- fread("../Data/9606.protein.links.v12.0.txt")
+# 
+# ppi$protein1 <- gsub("9606.", "", ppi$protein1, fixed = T)
+# ppi$protein2 <- gsub("9606.", "", ppi$protein2, fixed = T)
+# 
+# mart <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+# gene_info <- getBM(
+#   attributes = c("ensembl_peptide_id", "hgnc_symbol","chromosome_name","start"), mart = mart)
+# nam_chr <- c(as.character(c(1:22)),"X","Y","MT")
+# gene_info$chromosome_name <- factor(gene_info$chromosome_name , ordered = TRUE, 
+#        levels = c(nam_chr,sort(unique(gene_info$chromosome_name)[!unique(gene_info$chromosome_name)%in%nam_chr])))
+# save(gene_info,file="../Data/gene_info.RData")
+# 
+# ppi$gene1 <- gene_info$hgnc_symbol[match(ppi$protein1, gene_info$ensembl_peptide_id)]
+# ppi$gene2 <- gene_info$hgnc_symbol[match(ppi$protein2, gene_info$ensembl_peptide_id)]
+# save(ppi,file="../Data/ppi_w_symbols.RData")
 
-ppi <- fread("../Data/9606.protein.links.v12.0.txt")
-
-ppi$protein1 <- gsub("9606.", "", ppi$protein1, fixed = T)
-ppi$protein2 <- gsub("9606.", "", ppi$protein2, fixed = T)
-
-mart <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
-gene_info <- getBM(
-  attributes = c("ensembl_peptide_id", "hgnc_symbol"), mart = mart)
-
-ppi$gene1 <- gene_info$hgnc_symbol[match(ppi$protein1, gene_info$ensembl_peptide_id)]
-ppi$gene2 <- gene_info$hgnc_symbol[match(ppi$protein2, gene_info$ensembl_peptide_id)]
-
+load("../Data/ppi_w_symbols.RData")
+load("../Data/gene_info.RData")
 
 # Load data: expressions & scores ####
 # load("C:/Users/Dafydd/Documents/Projects/lasso_PPI/global.RData")
@@ -51,13 +57,13 @@ ok_cells <- intersect(
   rownames(ind_exp),
   rownames(dep_scor))
 
-X <- ind_exp[ok_cells, ]
-y <- dep_scor[ok_cells,]
+X_ind_exp <- ind_exp[ok_cells, ]
+y_dep_scor <- dep_scor[ok_cells,]
 if(exp_src=='rnaseq'){
-expressed <- apply(X, 2, function(x) mean(x > 0))
-X <- X[, expressed > 0.95]
-X <- apply(X, 2, function(x)
-  (x - mean(x))/sd(x))
+  expressed <- apply(X_ind_exp, 2, function(x) mean(x > 0))
+  X_ind_exp <- X_ind_exp[, expressed > 0.95]
+  X_ind_exp <- apply(X_ind_exp, 2, function(x)
+    (x - mean(x))/sd(x))
 }
 
 # test phi, corr vs rmse
@@ -74,8 +80,9 @@ gene_sample3 <-c("PALB2","BARD1","BRIP1","RAD50","RAD51C","RAD51D","RAD54L","ATM
 gene_sample3 <-gene_sample3[gene_sample3%in%colnames(dep_scor)]
 
 gene_sample4 <- c("MYC","SF3B1","BRCA1","BRCA2")
-
-
+# gene_sample5<- c("KRAS","TP53","RASGRF2", "NRAS",""
+gene_sample6<- c("EGFR","MYC","ERBB2", "CDK4/6","PIK3CA")
+                                  
 
 
 library("parallel")
@@ -87,8 +94,8 @@ phiout <- mclapply(gene_sample4,function(gene){
     NULL
   }else{
     print(paste('Processing',gene))
-    y <- na.omit(y[, gene])
-    X <- X[match(names(y),rownames(X)), ]
+    y <- na.omit(y_dep_scor[, gene])
+    X <- X_ind_exp[match(names(y),rownames(X_ind_exp)), ]
     scores <- get_scores(gene, ppi)
     out <- run_reg_lasso(
       X, y, scores,
@@ -97,3 +104,225 @@ phiout <- mclapply(gene_sample4,function(gene){
     return(out)
   }
 },mc.cores = 10)
+
+
+
+
+# PLOTTING ####
+
+
+tmp <- results_cnv$betas
+tmp$gene <- rownames(tmp)
+tmp$correl <- cor(X_cnv, y)[, 1]
+tmp$rank <- 1:nrow(tmp)
+
+tmp$label <- tmp$gene
+tmp$label[tmp$betas_pen == 0] <- NA
+tmp$label[tmp$gene == "SF3B1"] <- "SF3B1"
+
+
+glabels <- tmp[order(abs(tmp$betas_pen),decreasing = T)[1:20],]
+
+
+ggplot(tmp, aes(rank, correl, label = label)) +
+  geom_point() +
+  # geom_label(aes(size = abs(betas_pen))) +
+  theme_classic()+
+  ggrepel::geom_label_repel(aes(label=gene,size = abs(betas_pen)),data=glabels,
+                            box.padding = .3, max.overlaps = Inf, color = "blue2",
+                            seed = 0,min.segment.length = 0,nudge_y = .1,na.rm = TRUE
+  ) +
+  labs(title="SF3B1",x="Gene order",y="Correlation")
+
+
+
+
+# BELOW HERE IS TEMP ####
+# Load R libs ####
+library(caret)
+library(data.table)
+library(ggplot2)
+library(glmnet)
+
+
+# Source code ####
+source("allfunctions.R")
+
+
+# Load STRING data ####
+load("../Data/ppi_w_symbols.RData")
+
+
+# Load DepMap data ####
+load("../Data/global.RData")
+
+
+# Normalize RNA expression data ####
+X <- rnaseq
+expressed <- apply(X, 2, function(x) mean(x > 0))
+X <- X[, expressed > 0.95]
+X <- apply(X, 2, function(x)
+  (x - mean(x))/sd(x))
+X_rna <- X
+
+
+# Define CNV table ####
+X_cnv <- cnv
+X_cnv <- na.omit(X_cnv)
+
+
+# Define mutation table ####
+tmp <- fread("../Data/Damaging_Mutations.csv")
+mut <- data.matrix(tmp[,-1])
+mut <- apply(mut, 2, function(x)
+  as.numeric(x > 0))
+rownames(mut) <- tmp[[1]]
+X_mut <- mut
+
+
+# Define outcome ####
+gene_sample6<- c("EGFR","MYC","ERBB2", "CDK4","CDK6","PIK3CA")
+
+res_cnv <- lapply(gene_sample6[5],function(gene){
+  y <- demeter2[, gene]
+  # y <- kronos[, gene]
+  y <- y[!is.na(y)]
+  
+  tissue <- sample_info[names(y), "lineage"]
+  #y_resid <- residuals(lm(y ~ tissue))
+  #y <- y_resid
+  
+  
+  # Find overlapping cell lines ####
+  ok_cells <- intersect(names(y), rownames(X_rna))
+  ok_cells <- intersect(ok_cells, rownames(X_cnv))
+  ok_cells <- intersect(ok_cells, rownames(X_mut))
+  
+  
+  # Remove features without variance ####
+  X_rna  <- X_rna[ok_cells, ]
+  X_rna <- X_rna[, apply(X_rna, 2, var) > 0]
+  
+  X_mut <- X_mut[ok_cells, ]
+  X_mut <- X_mut[, colSums(X_mut) >= 5]
+  
+  X_cnv  <- X_cnv[ok_cells, ]
+  X_cnv <- X_cnv[, apply(X_cnv, 2, var) > 0]
+  
+  y <- y[ok_cells]
+  
+  
+  # Run LASSO ####
+  scores <- get_scores(gene, ppi)
+  
+  # results_rna <- run_reg_lasso(
+  #   X_rna, y, scores,
+  #   n_folds = 10, phi_range = seq(0, 1, length = 30))
+  
+  results_cnv <- run_reg_lasso(
+    X_cnv, y, scores,
+    n_folds = 10, phi_range = seq(0, 1, length = 30))
+  
+  # results_mut <- run_reg_lasso(
+  #   X_mut, y, scores,
+  #   n_folds = 10, phi_range = seq(0, 1, length = 30))
+  
+  
+  return(results_cnv)
+  
+});names(res_cnv)<-gene_sample6
+
+# plt ####
+lapply(names(res_cnv),function(nam){
+  ## prep objs ####
+  y <- demeter2[, nam]
+  # y <- kronos[, gene]
+  y <- y[!is.na(y)]
+  
+  tissue <- sample_info[names(y), "lineage"]
+  #y_resid <- residuals(lm(y ~ tissue))
+  #y <- y_resid
+  
+  
+  # Find overlapping cell lines ####
+  ok_cells <- intersect(names(y), rownames(X_rna))
+  ok_cells <- intersect(ok_cells, rownames(X_cnv))
+  ok_cells <- intersect(ok_cells, rownames(X_mut))
+  
+  
+  # Remove features without variance ####
+  X_rna  <- X_rna[ok_cells, ]
+  X_rna <- X_rna[, apply(X_rna, 2, var) > 0]
+  
+  X_mut <- X_mut[ok_cells, ]
+  X_mut <- X_mut[, colSums(X_mut) >= 5]
+  
+  X_cnv  <- X_cnv[ok_cells, ]
+  X_cnv <- X_cnv[, apply(X_cnv, 2, var) > 0]
+  
+  y <- y[ok_cells]
+  
+  ## Plot ####
+  
+  results_cnv <- res_cnv[[nam]]
+  tmp <- results_cnv$betas
+  tmp$gene <- rownames(tmp)
+  tmp$correl <- cor(X_cnv, y)[, 1]
+  tmp$rank <- 1:nrow(tmp)
+  
+  tmp$label <- tmp$gene
+  tmp$label[tmp$betas_pen == 0] <- NA
+  tmp$label[tmp$gene == nam] <- nam
+  
+  
+  glabels <- tmp[order(abs(tmp$betas_pen),decreasing = T)[1:20],]
+  # corr
+  aframe <- data.frame(
+    X_cnv[, c(nam, "KRAS", "NRAS","ALK")],
+    y
+  )
+  
+  out_plt <- ggplot(tmp, aes(rank, correl, label = label)) +
+    geom_point() +
+    # geom_label(aes(size = abs(betas_pen))) +
+    theme_classic()+
+    ggrepel::geom_label_repel(aes(label=gene,size = abs(betas_pen)),data=glabels,
+                              box.padding = .3, max.overlaps = Inf, color = "blue2",
+                              seed = 0,min.segment.length = 0,nudge_y = .1,na.rm = TRUE
+    ) +
+    annotate("text",x=2000,y=-.3,label=paste0("cor=",round(cor.test(aframe[,nam],aframe$y)$estimate,3),
+                                              "\npval=",round(cor.test(aframe[,nam],aframe$y)$p.value,5)))+
+    labs(title=nam,x="Gene order",y="Correlation")
+  if(scatterp){
+    aframe$CDK6_norm <- F
+    aframe$CDK6_norm[which(aframe$CDK6>=0.8&aframe$CDK6<=1.2)]<-T
+    ggplot(aframe,aes(y=y,x=aframe[,"ALK"],color=CDK6_norm))+
+      theme_classic()+      geom_hline(yintercept =0)+
+      geom_vline(xintercept =1)+geom_point()+
+      geom_smooth(method="lm",se=F)+
+      # scale_x_continuous(breaks = c(.25,.5,1,2,3,4),labels =c("1/4",'1/2','1','2','3','4'),trans="log2")+
+      labs(title="ALK CNV vs CDK6 dependency",x=paste("ALK (CNV)"),y=paste("CDK6 (Dependency)"))
+    
+    # plot only normal 
+    aframe2 <- aframe[which(aframe$CDK6_norm),]
+    ggplot(aframe2,aes(y=y,x=aframe2[,"ALK"],color=CDK6_norm))+
+      theme_classic()+      
+      geom_hline(yintercept =0)+
+      geom_vline(xintercept =1)+geom_point()+
+      geom_smooth(method="lm",se=F)+
+      annotate("text",x=3,y=.2,label=paste0("cor=",round(cor.test(aframe2[,"ALK"],aframe2$y)$estimate,3),
+                                            "\npval=",round(cor.test(aframe2[,"ALK"],aframe2$y)$p.value,5)))+
+      labs(title="ALK CNV vs CDK6 dependency\npopulation=CDK6 CNV [0.8-1.2]",x=paste("ALK (CNV)"),y=paste("CDK6 (Dependency)"))
+    
+    
+    ggplot(aframe, aes(interaction(ALK > 1.2, CDK6 > 1.2), y)) + geom_boxplot()
+    aframe$ALK_norm<-F; aframe$ALK_norm[which(aframe$ALK>=0.8&aframe$ALK<=1.2)]<-T
+    ggplot(aframe,aes(x=y,color=ALK_norm,fill=CDK6_norm,group=paste(ALK_norm,CDK6_norm)))+geom_histogram(position = 'identity',alpha=0.5)+
+      geom_vline(aes(xintercept=mean(y)))+
+      theme_classic()+
+      facet_grid(paste(CDK6_norm,ALK_norm)~.)
+    
+  }
+  # return(out_plt)
+  ggsave(paste0("../Outputs/graphics/",nam,"_kronos.pdf"),out_plt,width = 8,height = 4)
+})
---
+++ Code/lasso_phi_v2_miko.R
@@ -0,0 +1,251 @@
+# Load R libs ####
+library(caret)
+library(data.table)
+library(ggplot2)
+library(glmnet)
+
+
+# Source code ####
+source("allfunctions.R")
+
+
+# Load STRING data ####
+load("../Data/ppi_w_symbols.RData")
+# Loadd Gene infrmation data ####
+load("../Data/gene_info.RData")
+
+# Load DepMap data ####
+load("../Data/global.RData")
+
+
+# Normalize RNA expression data ####
+X <- rnaseq
+expressed <- apply(X, 2, function(x) mean(x > 0))
+X <- X[, expressed > 0.95]
+X <- apply(X, 2, function(x)
+  (x - mean(x))/sd(x))
+X_rna <- X
+
+
+# Define CNV table ####
+X_cnv <- cnv
+X_cnv <- na.omit(X_cnv)
+
+
+# Define mutation table ####
+tmp <- fread("../Data/Damaging_Mutations.csv")
+mut <- data.matrix(tmp[,-1])
+mut <- apply(mut, 2, function(x)
+  as.numeric(x > 0))
+rownames(mut) <- tmp[[1]]
+X_mut <- mut
+
+
+# Define outcome ####
+gene_sample6<- c("PRMT5","EGFR","MYC","ERBB2", "CDK4","CDK6","PIK3CA")
+
+res_cnv <- lapply(gene_sample6,function(gene,scorObj='demeter2'){
+  save_results_file <- paste0(gene,'_cnv.RData')
+  save_results_file_path <-paste0("../Outputs/",scorObj,"/",save_results_file)
+  if(save_results_file%in%list.files(paste0("../Outputs/",scorObj,"/"))){
+    load(save_results_file_path)
+    return(results_cnv)
+    }else{
+  
+  
+  if(scorObj=='demeter2'){
+    y <- demeter2[, gene]
+  } else if(scorObj=='kronos'){
+    y <- kronos[, gene]
+  }
+y <- y[!is.na(y)]
+
+tissue <- sample_info[names(y), "lineage"]
+#y_resid <- residuals(lm(y ~ tissue))
+#y <- y_resid
+
+
+# Find overlapping cell lines ####
+ok_cells <- intersect(names(y), rownames(X_rna))
+ok_cells <- intersect(ok_cells, rownames(X_cnv))
+ok_cells <- intersect(ok_cells, rownames(X_mut))
+
+
+# Remove features without variance ####
+X_rna  <- X_rna[ok_cells, ]
+X_rna <- X_rna[, apply(X_rna, 2, var) > 0]
+
+X_mut <- X_mut[ok_cells, ]
+X_mut <- X_mut[, colSums(X_mut) >= 5]
+
+X_cnv  <- X_cnv[ok_cells, ]
+X_cnv <- X_cnv[, apply(X_cnv, 2, var) > 0]
+
+y <- y[ok_cells]
+
+
+# Run LASSO ####
+scores <- get_scores(gene, ppi)
+
+# results_rna <- run_reg_lasso(
+#   X_rna, y, scores,
+#   n_folds = 10, phi_range = seq(0, 1, length = 30))
+
+results_cnv <- run_reg_lasso(
+  X_cnv, y, scores,
+  n_folds = 10, phi_range = seq(0, 1, length = 30))
+
+# results_mut <- run_reg_lasso(
+#   X_mut, y, scores,
+#   n_folds = 10, phi_range = seq(0, 1, length = 30))
+
+save(results_cnv,file=save_results_file_path)
+return(results_cnv)
+}
+});names(res_cnv)<-gene_sample6
+
+
+# plt ####
+lapply(names(res_cnv),function(nam,scorObj='demeter2',res_cnv=res_cnv){
+ ## prep objs ####
+  if(scorObj=='demeter2'){
+    y <- demeter2[, nam]
+  } else if(scorObj=='kronos'){
+    y <- kronos[, nam]
+  }
+  y <- y[!is.na(y)]
+  
+  tissue <- sample_info[names(y), "lineage"]
+  #y_resid <- residuals(lm(y ~ tissue))
+  #y <- y_resid
+  
+  
+  # Find overlapping cell lines ####
+  ok_cells <- intersect(names(y), rownames(X_rna))
+  ok_cells <- intersect(ok_cells, rownames(X_cnv))
+  ok_cells <- intersect(ok_cells, rownames(X_mut))
+  
+  
+  # Remove features without variance ####
+  X_rna  <- X_rna[ok_cells, ]
+  X_rna <- X_rna[, apply(X_rna, 2, var) > 0]
+  
+  X_mut <- X_mut[ok_cells, ]
+  X_mut <- X_mut[, colSums(X_mut) >= 5]
+  
+  X_cnv  <- X_cnv[ok_cells, ]
+  X_cnv <- X_cnv[, apply(X_cnv, 2, var) > 0]
+  
+  y <- y[ok_cells]
+  
+  ## Plot ####
+
+  results_cnv <- res_cnv[[nam]]
+  tmp <- results_cnv$betas
+  tmp$gene <- rownames(tmp)
+  tmp$correl <- cor(X_cnv, y)[, 1]
+  # reorder
+  tmp <- cbind(tmp,gene_info[match(rownames(tmp),gene_info$hgnc_symbol),c("chromosome_name", "start_position")])
+  tmp<- tmp[order(tmp$chromosome_name,tmp$start_position),]
+  tmp$rank <- 1:nrow(tmp)
+
+  tmp$label <- tmp$gene
+  tmp$label[tmp$betas_pen == 0] <- NA
+  tmp$label[tmp$gene == nam] <- nam
+
+
+  glabels <- tmp[order(abs(tmp$betas_pen),decreasing = T)[1:20],]
+  glabels <- tmp[order(abs(tmp$betas_pen),decreasing = T)[1:20],]
+  # corr
+  aframe <- data.frame(
+    X_cnv[, c(nam, "KRAS", "NRAS","ALK")],
+    y
+  )
+
+  # remove X,Y and non cononical chromosomes
+  tmp <- tmp[tmp$chromosome_name %in% c(1:22),]
+  shaded_chrs <- c(as.character(seq(1,22,by=2)),'X',"MT")
+  shaded_chrs <- shaded_chrs[shaded_chrs%in% unique(tmp$chromosome_name)]
+  shad_block <- do.call(rbind,lapply(shaded_chrs,function(x){
+    data.frame(chr=x,xmin=tmp[which(tmp$chromosome_name==x),"rank"][1],xmax=rev(tmp[which(tmp$chromosome_name==x),"rank"])[1])
+  }))
+  Y_min <- min(tmp$correl)-0.05
+  Y_max <- max(tmp$correl)+0.05
+  
+  out_plt <- ggplot(tmp, aes(rank, correl, label = label)) +
+    annotate("rect", xmin =shad_block$xmin, xmax = shad_block$xmax, ymin = Y_min, ymax = Y_max,alpha = .2)+
+    geom_point(alpha=0.5) +
+    theme_classic()+
+    ggrepel::geom_label_repel(aes(label=gene,size = abs(betas_pen)),data=glabels,
+                              box.padding = .3, max.overlaps = Inf, color = "blue2",
+                              seed = 0,min.segment.length = 0,nudge_y = .1,na.rm = TRUE
+    ) +
+    theme(plot.margin=margin(0,0,0,0))+
+    scale_size_continuous("Beta magnitude")+
+    # annotate("text",x=2000,y=-.3,label=paste0("cor=",round(cor.test(aframe[,nam],aframe$y)$estimate,3),
+    #                                         "\npval=",round(cor.test(aframe[,nam],aframe$y)$p.value,5)))+
+    scale_x_continuous(labels=shaded_chrs, breaks=rowSums(shad_block[,-1])/2)+
+    labs(title=paste(nam,scorObj,"score"),x="Chromosome/Gene order",y="Correlation")
+  ggsave(paste0("../Outputs/graphics/",nam,"_",scorObj,"nolab.pdf"),out_plt,width = 12,height = 6)
+  ggsave(paste0("../Outputs/graphics/",nam,"_",scorObj,"nolab.JPEG"),out_plt,width = 12,height = 6)
+  ### Scatter or boxplots explaining primary peak (EGFR amplification is associated with increased dependency)
+  # out_plt+ xlim(c(which(tmp$gene=="EGFR")-100,which(tmp$gene=="EGFR")+100))#+ylim()
+  # out_plt+  geom_magnify(from = c(1, 0, 5, 1500),to = c(0, 10000, 2.5, 20000),shadow = TRUE)
+  # Plot whole chrom
+  gene_interest <- nam
+  which_interest <- which(tmp$chromosome_name==tmp$chromosome_name[which(tmp$gene==gene_interest)])
+  tmpsub <- tmp[which_interest,]
+  out_plt + coord_cartesian(xlim=c(min(which_interest),max(which_interest)),
+                            ylim=c(min(tmpsub$correl)-0.05,max(tmpsub$correl)+0.05),
+                            expand = F)+
+    ggrepel::geom_label_repel(aes(label=gene,size = abs(betas_pen)),data=glabels,
+                              box.padding = .3, max.overlaps = Inf, color = "blue2",
+                              seed = 0,min.segment.length = 0,nudge_y = .05,na.rm = TRUE
+    ) +
+    labs(title=paste(nam,scorObj,"score\nChr",unique(tmpsub$chromosome_name),"only"))
+  ggsave(paste0("../Outputs/graphics/",nam,"_",scorObj,"_zoom_",gene_interest,".pdf"),width = 12,height = 6)
+  ggsave(paste0("../Outputs/graphics/",nam,"_",scorObj,"_zoom_",gene_interest,".JPEG"),width = 12,height = 6)
+  ### Zoom into GAB2 region. Show that many genes have similar statistical evidence but lasso identifies GAB2.
+  gene_interest <- 'GAB2'
+  which_interest <- which(tmp$gene==gene_interest)
+  tmpsub <- tmp[(which_interest-100):(which_interest+100),]
+  out_plt + coord_cartesian(xlim=c(which_interest-100,which_interest+100),
+                            ylim=c(min(tmpsub$correl)-0.05,max(tmpsub$correl)+0.05),
+                            expand = F)+
+    ggrepel::geom_label_repel(aes(label=gene,size = abs(betas_pen)),data=glabels,
+                              box.padding = .3, max.overlaps = Inf, color = "blue2",
+                              seed = 0,min.segment.length = 0,nudge_y = .05,na.rm = TRUE
+    ) +
+    labs(title=paste(nam,scorObj,"score\n+/- 100 sorrounding genes"))
+  ggsave(paste0("../Outputs/graphics/",nam,"_",scorObj,"_zoom_",gene_interest,".pdf"),width = 12,height = 6)
+  ggsave(paste0("../Outputs/graphics/",nam,"_",scorObj,"_zoom_",gene_interest,".JPEG"),width = 12,height = 6)
+  
+  
+  if(scatterp){
+    aframe$CDK6_norm <- F
+    aframe$CDK6_norm[which(aframe$CDK6>=0.8&aframe$CDK6<=1.2)]<-T
+    ggplot(aframe,aes(y=y,x=aframe[,"ALK"],color=CDK6_norm))+
+      theme_classic()+      geom_hline(yintercept =0)+
+      geom_vline(xintercept =1)+geom_point()+
+      geom_smooth(method="lm",se=F)+
+      # scale_x_continuous(breaks = c(.25,.5,1,2,3,4),labels =c("1/4",'1/2','1','2','3','4'),trans="log2")+
+      labs(title="ALK CNV vs CDK6 dependency",x=paste("ALK (CNV)"),y=paste("CDK6 (Dependency)"))
+    
+    # plot only normal 
+    aframe2 <- aframe[which(aframe$CDK6_norm),]
+    ggplot(aframe2,aes(y=y,x=aframe2[,"ALK"],color=CDK6_norm))+
+      theme_classic()+      
+      geom_hline(yintercept =0)+
+      geom_vline(xintercept =1)+geom_point()+
+      geom_smooth(method="lm",se=F)+
+      annotate("text",x=3,y=.2,label=paste0("cor=",round(cor.test(aframe2[,nam],aframe2$y)$estimate,3),
+                                                "\npval=",round(cor.test(aframe2[,nam],aframe2$y)$p.value,5)))+
+      labs(title="ALK CNV vs CDK6 dependency\npopulation=CDK6 CNV [0.8-1.2]",x=paste("ALK (CNV)"),y=paste("CDK6 (Dependency)"))
+    
+    
+    
+    
+  }
+  # return(out_plt)
+
+})
